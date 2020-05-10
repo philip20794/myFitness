d9b3088d2cb0c991273fbbde89ec337be8255dc9
Spahn <Philip.Spahn@adesso.de>
2020-05-10 20:03:15 +0200

    test4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -17,21 +17,28 @@ export class Tab1Page {
   currentWeek: Week;
 
   constructor(public storage: Storage) {
+    console.log('cnstruckt');
     this.loaded = false;
     this.genOldStuff();
+    console.log('-----------------DONE-------------');
   }
 
   ionViewDidEnter() {
+    console.log('enter view');
     this.loaded = false;
     this.init();
+    console.log('-----------------DONE-------------');
   }
 
   genOldStuff() {
+    console.log('gend old stuff');
     this.storage.set('2020week17', new Week([], 17, new Day(null, 66.16, 18, 43)));
     this.storage.set('2020week18', new Week([], 18, new Day(null, 66.63, 17.7, 43.1)));
+    console.log('-----------------DONE-------------');
   }
 
   async init() {
+    console.log('init');
     await this.storage.get(this.today.getFullYear() + 'week' + this.currentWeekNumber()).then(value => {
       if (value !== null) {
         this.currentWeek = new Week(value.days, value.nr, value.durchschnitt);
@@ -43,21 +50,27 @@ export class Tab1Page {
       this.currentWeek.days.push(new Day(this.today, 0, 0, 0));
     }
     this.loaded = true;
+    console.log('-----------------DONE-------------');
   }
 
   save() {
+    console.log('save');
     this.genAverage();
     this.storage.set(this.today.getFullYear() + 'week' + this.currentWeekNumber(), this.currentWeek);
+    console.log('-----------------DONE-------------');
   }
 
   focusNext(event: any, nextElement: any) {
+    console.log('fokus next');
     const enterClicked = event.key === 'Enter';
     if (enterClicked) {
       nextElement.setFocus();
     }
+    console.log('-----------------DONE-------------');
   }
 
   genAverage() {
+    console.log('genavareg');
     if (this.currentWeek.days === null) {return; }
     if (this.currentWeek.days.length < 1) {return; }
     let gewicht = 0;
@@ -75,6 +88,7 @@ export class Tab1Page {
     this.currentWeek.durchschnitt.gewicht = (gewicht / days);
     this.currentWeek.durchschnitt.fat = (fat / days);
     this.currentWeek.durchschnitt.muskel = (muskel / days);
+    console.log('-----------------DONE-------------');
   }
 
 
